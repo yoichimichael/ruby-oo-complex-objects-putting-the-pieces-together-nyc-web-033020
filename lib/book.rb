eb6a096daaba447b336f5cf1cc2d6555563f7cf1
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-03-31 15:04:29 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,57 @@
+class Book
+
+	def initialize(title)
+		@title = title
+	end
+     #getter/reader method
+	def title
+		@title
+	end
+	
+		#setter/writer method
+	def author=(author)
+    @author = author
+	end
+		#getter/reader method
+	def author
+    @author
+	end
+			#setter/writer method
+	def page_count=(num)
+    @page_count = num
+  end
+			#getter/reader method
+  def page_count
+    @page_count
+	end
+	
+	def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+
+	def turn_page
+		puts "Flipping the page...wow, you read fast!"
+  end
+
+end
 
 
+# book.rb
+ 
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
